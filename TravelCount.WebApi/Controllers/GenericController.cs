b9fd6c76ee67486155b7ed36274a023cdb446846
maHlavacek <martin_hlavacek@icloud.com>
2020-01-14 21:04:30 +0100

    Generic controller added

---
+++ TravelCount.WebApi/Controllers/GenericController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelCount.WebApi.Controllers
+{
+    public abstract class GenericController<I, M> : ControllerBase
+        where M : Transfer.TransferObject, I, Contracts.ICopyable<I>, new()
+        where I : Contracts.IIdentifiable
+    {
+    }
+}
